3f658172de644fb047ad2cbd0bccfa1c84674ec0
Iash Bashir <Iash.Bashir@sea.gloal.com>
2024-02-06 16:07:30 +0800

    Refactor fin flutter calculations and update input values

--- script/bennet_fin_flutter_original.py
+++ script/bennet_fin_flutter_original.py
@@ -16,28 +16,24 @@ if peregrine_rocket.is_file(): 	# if input file exists, use it
     # Read in the config file
     from peregrine_rocket import *
 else:  # no input file exists; initialize input variables from the values below
-	# Note: if input file exists, all of the values below will be ignored
-    VERBOSE = 0    # '0' only prints final results; 1 prints intermediate results
+    VERBOSE = 1    # '0' only prints final results; 1 prints intermediate results 
     # Inputs to be provided
-    Units = "Imperial"  # "Imperial" = inches,feet,psi,deg.F; "SI" = cm,meters,KPa,deg.C
+    Units = "SI"  # "Imperial" = inches,feet,psi,deg.F; "SI" = cm,meters,KPa,deg.C
     # Input provided below must match selection above
     # Rocket and Launch Site Specs
-    MaxV = 1500         # maximum predicted rocket velocity
-    # predicted altitude at predicted maximum rocket velocity (AGL)
-    AMaxV = 14000
-    LSA = 4500          # launch site altitude (ASL)
-    # Temp (Fahrenheit or Centigrade, depending upon selected units)
-    TLS = 65
-    # Use Default Sea-Level Temp ("DST") or Launch Site Temp ("LST")
-    DEF = "DST"
-    # Fin Specs
-    Thickness = 0.1875  # Fin Thickness
-    TC = 2.5             # if TC == -1, calculate TC (or a Pseudo TC)
-    RC = 7.5            # Root Chord length
-    Height = 3          # Fin Height
-    GE = 600000         # Shear Modulus
-    T2T = "NO"         # Tip-to-Tip reinforcing present? "YES" or "NO"
-    Fin_Vertex_File_Name = "trap.csv"  # Name of csv file output from OpenRocket
+    MaxV = 457.2       # maximum predicted rocket velocity
+    AMaxV = 4267.2     # predicted altitude at predicted maximum rocket velocity (AGL)
+    LSA = 1371.6       # launch site altitude (ASL)
+    TLS = 18.333       # Temp (Fahrenheit or Centigrade, depending upon selected units)
+    DEF = "DST"        # Use Default Sea-Level Temp ("DST") or Launch Site Temp ("LST")
+    #Fin Specs
+    Thickness = 0.47625   # Fin Thickness
+    TC = 6.35             # if TC == -1, calculate TC (or a Pseudo TC)
+    RC = 19.05            # Root Chord length
+    Height = 7.62         # Fin Height
+    GE = 4136854          # Shear Modulus
+    T2T  = "YES"          # Tip-to-Tip reinforcing present? "YES" or "NO"
+    Fin_Vertex_File_Name = "trap_SI.csv" # Name of csv file output from OpenRocket
 # End of Inputs
 
 # Read in the vertex file exported by OpenRocket (see articles for explanation)
--- script/bennet_fin_flutter_refactored.py
+++ script/bennet_fin_flutter_refactored.py
@@ -1,31 +1,7 @@
-from sympy import symbols, lambdify, sqrt, exp, pi, cos
-import numpy as np
-import math
+from sympy import symbols, lambdify, sqrt, pi
 
 # Define symbols
-MaxV, AMaxV, LSA, TLS, DEF, Thickness, TC, RC, Height, GE, T2T, fin_area, fin_Cx = symbols(
-    'MaxV AMaxV LSA TLS DEF Thickness TC RC Height GE T2T fin_area fin_Cx')
-airP, Temp, Spd_of_Sound, Term1, Term2, Term3, Vf = symbols(
-    'airP Temp Spd_of_Sound Term1 Term2 Term3 Vf')
+MaxV, AMaxV, LSA, TLS, Thickness, TC, RC, Height, GE, fin_area, fin_Cx, Temp, Spd_of_Sound, airP, etc. = symbols('MaxV AMaxV LSA TLS Thickness TC RC Height GE fin_area fin_Cx Temp Spd_of_Sound airP etc.')
 
-# Expressions for Fin Area and Center of Pressure (Cx)
-# These would be computed from the given fin vertex data
-# For now, let's assume they are inputs
-
-# Expressions for Fin Flutter Velocity
-Fin_Eps_expr = (fin_Cx / RC) - 0.25
-ThicknessRatio_expr = Thickness / RC
-Lambda_expr = TC / RC
-AspectRatio_expr = (Height**2) / fin_area
-DN_expr = (24 * Fin_Eps_expr * 1.4 * airP) / pi
-Term1_expr = (DN_expr * AspectRatio_expr**3) / \
-    (ThicknessRatio_expr**3 * (AspectRatio_expr + 2))
-Term2_expr = (Lambda_expr + 1)/2
-Term3_expr = airP / airP  # Assuming sea-level pressure for simplicity
-Vf_expr = Spd_of_Sound * sqrt(GE/(Term1_expr * Term2_expr * Term3_expr))
-
-# Lambdify expressions for numerical calculations
-fin_area_and_cx_function = lambdify(
-    (fin_area, fin_Cx), (fin_area, fin_Cx), modules='numpy')
-flutter_velocity_function = lambdify(
-    (airP, Temp, Spd_of_Sound, GE, RC, Height, TC, Thickness, fin_Cx), Vf_expr, modules='numpy')
+# Define expressions for each calculation
+# Example: Fin_Eps_expr, ThicknessRatio_expr, Lambda_expr, AspectRatio_expr, etc.
