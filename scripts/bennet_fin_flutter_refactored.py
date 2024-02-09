d61b55176893847e4d437779f3ccbe4d0a6ffbe3
Iash Bashir <Iash.Bashir@sea.gloal.com>
2024-02-09 09:22:20 +0800

    Deleted unused scripts

--- scripts/bennet_fin_flutter_refactored.py
+++ scripts/bennet_fin_flutter_refactored.py
@@ -21,7 +21,6 @@ thickness_ratio_expr = t/cr
 T_expr_SI = 15-0.0065*h
 T_expr_Imperial = 59 - 0.00356 * h
 
-
 # Pressure
 P_expr_SI = 101.325 * ((T + 273.16) / 288.16) ** 5.256
 P_expr_Imperial = 14.696 * ((T + 459.7) / 518.7) ** 5.256
@@ -42,13 +41,6 @@ Term1_expr = (DN_expr*AR_expr**3)/((thickness_ratio_expr**3)*(AR_expr+2))
 Term2_expr = (lambda_expr+1)/2
 Term3_expr_SI = P_expr_SI/101.325
 Term3_expr_Imperial = P_expr_Imperial/14.696
-
-
-# Fin flutter velocity expression
-# Fin flutter velocity expression
-# Vf_expr_SI = a_expr_SI * (G / (Term1_expr * Term2_expr * Term3_expr_SI))**0.5
-# Vf_expr_Imperial = a_expr_Imperial * \
-#     (G / (Term1_expr * Term2_expr * Term3_expr_Imperial))**0.5
 
 Vf_expr = a*(G/(T1 * T2 * T3))**0.5
 
@@ -98,13 +90,10 @@ def evaluate_flutter_velocity(G_val, cr_val, ct_val, b_val, t_val, m_val, P_val,
         epsilon: epsilon_val
     })
     term2 = Term2_expr.subs({cr: cr_val, ct: ct_val})
-    # term3 = Term3_expr_Imperial.subs({T: T_val, P: P_val})
     
+    # term3 = Term3_expr_Imperial.subs({T: T_val, P: P_val})
     term3 = Term3_expr_SI.subs({T: T_val, P: P_val})
-#     print(f"term1={term1}")
-#     print(f"term2={term2}")
-#     print(f"term3={term3}")
-
+    
     vf_expr_evaluated = Vf_expr.subs(
         {a: a_val, G: G_val, T1: term1, T2: term2, T3: term3})
     # Numerical evaluation
@@ -123,37 +112,9 @@ def compute_and_print_flutter_velocity(unit_system, cr_val, ct_val, b_val, h_val
     temperature = temperature_function(h_val)
     pressure = pressure_function(temperature)
     speed_of_sound = speed_of_sound_function(temperature)
-
-#     # Print intermediate values for verification
-#     print(
-#         f"Temperature = {temperature} {'Celsius' if unit_system == 'si' else 'Fahrenheit'}")
-#     print(f"Pressure = {pressure} {'kPa' if unit_system == 'si' else 'psi'}")
-#     print(
-#         f"Speed of Sound = {speed_of_sound} {'m/s' if unit_system == 'si' else 'ft/s'}")
-
     # Evaluate and compute flutter velocity
     flutter_velocity = evaluate_flutter_velocity(
         G_val, cr_val, ct_val, b_val, t_val, m_val, pressure, K_val, P0_val, speed_of_sound, temperature)
 
-#     # Print computed flutter velocity
-#     print(
-#         f"Computed Flutter Velocity: {flutter_velocity} {'m/s' if unit_system == 'si' else 'ft/s'}")
     return flutter_velocity
 
-
-# Example usage
-# if __name__ == "__main__":
-#     unit_system = 'si'  # or 'Imperial'
-#     cr_val = 30.0  # Root chord length
-#     ct_val = 10.0  # Tip chord length
-#     b_val = 14.0  # Wingspan
-#     h_val = 0 + 3048.0  # Altitude in meters or feet
-#     t_val = 0.4  # Thickness
-#     m_val = 9.0  # Sweep length in cm or inches
-#     G_val = 5000000.0  # Shear modulus in kPa or psi
-#     K_val = 1.4  # Specific heat ratio
-#     P0_val = 101.325  # Standard pressure in kPa or psi
-
-#     # Call the encapsulated method
-#     compute_and_print_flutter_velocity(
-#         unit_system, cr_val, ct_val, b_val, h_val, t_val, m_val, G_val, K_val, P0_val)
